4c32b0359ad5c997983a8b6d48d5216aeb90fc95
cy3erdr4g0n <imrg10400@gmail.com>
2024-10-16 03:43:30 +0100

    status code

--- utils/error.js
+++ utils/error.js
@@ -1,14 +1,14 @@
 class AppError extends Error {
   constructor(message, statusCode) {
     super(message);
-    this.statusCode = statusCode;
+    this.statusCode = 400;
   }
 }
 
 const errorHandler = async (err, res) => {
   const statusCode = err.statusCode || 500;
 
-  res.status(statusCode).json({
+  res.status(500).json({
     message: err.message,
   });
 };
